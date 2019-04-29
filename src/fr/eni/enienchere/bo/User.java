4b71043184a2768da0608131cf5d8478b7b1f03e
Emeline <emeline.hourmand@gmail.com>
2019-04-29 13:59:02 +0200

    update User.java delete ArticleDAOJdbcImpl delete UserDAOJdbcImpl update ArticleDAO

--- src/fr/eni/enienchere/bo/User.java
+++ src/fr/eni/enienchere/bo/User.java
@@ -1,25 +1,54 @@
 package fr.eni.enienchere.bo;
 
 import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  *
  * @author ehourman2019
  *
  */
 @Data
-public class User {
+@Entity(name = "UTILISATEURS")
+public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no_utilisateur")
     private Long idUser;
-    private String userName;
-    private String lastName;
-    private String firstName;
-    private String email;
-    private int phoneNumber;
-    private String street;
-    private String postalCode;
-    private String city;
-    private String password;
-    private Long money;
-    private boolean userAdmin;
 
+    @Column(name = "pseudo")
+    private String userName;
+
+    @Column(name = "nom")
+    private String lastName;
+
+    @Column(name = "prenom")
+    private String firstName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telephone")
+    private int phoneNumber;
+
+    @Column(name = "rue")
+    private String street;
+
+    @Column(name = "code_postal")
+    private String postalCode;
+
+    @Column(name = "ville")
+    private String city;
+
+    @Column(name = "mot_de_passe")
+    private String password;
+
+    @Column(name = "credit")
+    private Long money;
+
+    @Column(name = "administrateur")
+    private boolean userAdmin;
 }
--- src/fr/eni/enienchere/dal/dao/ArticleDAO.java
+++ src/fr/eni/enienchere/dal/dao/ArticleDAO.java
@@ -1,7 +1,5 @@
 package fr.eni.enienchere.dal.dao;
 
-import fr.eni.enienchere.dal.dao.JdbcImpl.AricleDAOJdbcImpl;
-
 /**
  *
  * @author ehourman2019
