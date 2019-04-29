80992e2c472614c7815c8897edd86b9bd699186b
Emeline <emeline.hourmand@gmail.com>
2019-04-29 11:52:03 +0200

    create Userdao and userDaoJdbcImpl

---
+++ src/fr/eni/enienchere/dal/dao/JdbcImpl/UserDAOJdbcImpl.java
@@ -0,0 +1,11 @@
+package fr.eni.enienchere.dal.dao.JdbcImpl;
+
+import fr.eni.enienchere.dal.dao.UserDAO;
+
+/**
+ *
+ * @author ehourman2019
+ *
+ */
+public class UserDAOJdbcImpl implements UserDAO {
+}
---
+++ src/fr/eni/enienchere/dal/dao/UserDAO.java
@@ -0,0 +1,9 @@
+package fr.eni.enienchere.dal.dao;
+
+/**
+ *
+ * @author ehourman2019
+ *
+ */
+public interface UserDAO {
+}
