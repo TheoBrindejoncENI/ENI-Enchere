47671ed94bb2cde154a10939586221d6d6d5fd78
Emeline <emeline.hourmand@gmail.com>
2019-04-29 16:07:08 +0200

    update category.java

--- src/fr/eni/enienchere/bo/Category.java
+++ src/fr/eni/enienchere/bo/Category.java
@@ -1,15 +1,24 @@
 package fr.eni.enienchere.bo;
 
 import lombok.Data;
+
+import javax.persistence.*;
+
 /**
  *
  * @author ehourman2019
  *
  */
 @Data
+@Entity(name = "CATEGORIES")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no_categorie")
     private Long idCategory;
-    private String label;
+
+    @Column(name = "libelle")
+    private String title;
 
 }
