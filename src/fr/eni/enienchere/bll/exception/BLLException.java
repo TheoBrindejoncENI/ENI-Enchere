5efd20e580c986b63ebc7a80ffd14caaf70d050a
Emeline <emeline.hourmand@gmail.com>
2019-04-29 11:45:54 +0200

    update BLLException.java

--- src/fr/eni/enienchere/bll/exception/BLLException.java
+++ src/fr/eni/enienchere/bll/exception/BLLException.java
@@ -3,17 +3,34 @@ package fr.eni.enienchere.bll.exception;
 @SuppressWarnings("serial")
 public class BLLException extends Exception{
 
+    /**
+     * Empty Constructor
+     *
+     */
     public BLLException() {
     }
 
+    /**
+     * Constructor with message
+     * @param messaqe
+     */
     public BLLException(String messaqe) {
         super(messaqe);
     }
 
+    /**
+     * Constructor with message and exception
+     * @param message
+     * @param exception
+     */
     public BLLException(String message, Throwable exception) {
         super(message, exception);
     }
 
+    /**
+     * Build the message
+     * @return string message
+     */
     @Override
     public String getMessage() {
         StringBuffer sb = new StringBuffer("Couche BLL - ");
