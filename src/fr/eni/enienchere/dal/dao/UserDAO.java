ef14da8a991e0f5a465c14c20716bfa815e7cad3
Emeline <emeline.hourmand@gmail.com>
2019-04-29 11:59:59 +0200

    update UserDAO

--- src/fr/eni/enienchere/dal/dao/UserDAO.java
+++ src/fr/eni/enienchere/dal/dao/UserDAO.java
@@ -1,9 +1,51 @@
 package fr.eni.enienchere.dal.dao;
 
+import fr.eni.enienchere.bo.User;
+import fr.eni.enienchere.dal.exception.DALException;
+
+import java.util.List;
+
 /**
  *
  * @author ehourman2019
  *
  */
 public interface UserDAO {
+
+    /**
+     * Insert a user in bdd
+     * @param user
+     * @throws DALException
+     */
+    void insert(User user) throws DALException;
+
+    /**
+     * Select all user in bdd
+     * @return
+     * @throws DALException
+     */
+    List<User> selectAll() throws DALException;
+
+    /**
+     * Select user with id
+     * @param id
+     * @return
+     * @throws DALException
+     */
+    User selectById(int id) throws DALException;
+
+    /**
+     * Update user in bdd
+     * @param user
+     * @throws DALException
+     */
+    void update(User user) throws DALException;
+
+    /**
+     * Delete user in bdd
+     * @param id
+     * @throws DALException
+     */
+    void delete(int id) throws DALException;
+
 }
