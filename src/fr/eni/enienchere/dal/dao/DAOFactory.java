3a48a6db0f0db4d707d20e10a41db9a8ce055f59
Emeline <emeline.hourmand@gmail.com>
2019-04-30 13:33:17 +0200

    update DAOFactory

--- src/fr/eni/enienchere/dal/dao/DAOFactory.java
+++ src/fr/eni/enienchere/dal/dao/DAOFactory.java
@@ -14,4 +14,8 @@ public class DAOFactory {
     public static  ArticleDAO getArticleDAO() {
         return new ArticleDAO();
     }
+
+    public static CategoryDAO getCategoryDAO() {
+        return  new CategoryDAO();
+    }
 }
